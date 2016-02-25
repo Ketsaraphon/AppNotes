f32564a690caede3995d93216e457e98485caec7
Ketsaraphon Bunyuen <kbunyuen@gmail.com>
2016-02-25 13:11:04 +0700

    โปรแกรม AppNotes สำหรับฝึก  debug

---
+++ AppNote Ketsaraphon/MakeSchoolNotes/ViewControllers/NewNoteViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NewNoteViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by WebEngineering on 2/4/2559 BE.
+//  Copyright © 2559 Chris Orcutt. All rights reserved.
+//
+
+import UIKit
+
+class NewNoteViewController: UIViewController {
+    
+    var currentNote: Note?
+
+    override func viewDidLoad() { //ทำเมื่อโหลดหน้า
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) { //ฟังชันนี้ทำงานทุกครั้ง ก่อนที่จะเปลี่ยนไปอีกหน้านึง คือกดปุ่ม
+        
+       // currentNote = Note() //create object Note
+        //currentNote!.title = "New note test"
+       // currentNote!.content = "Dummy content"
+        
+        if(segue.identifier == "ShowNewNote"){
+            currentNote = Note() //สร้าง note เปล่าๆขึ้นมา
+            let noteDisplay = segue.destinationViewController as! NoteDisplayViewController
+            noteDisplay.note = currentNote
+        }
+    }
+
+
+}
